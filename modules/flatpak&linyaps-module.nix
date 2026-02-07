09c4fc83853bd7c0dc50e3686386431e342103e6
xuqihao <xuqihao@ihep.ac.cn>
2026-02-07 22:45:24 +0800

    add some gnome experimental-features

--- modules/flatpak&linyaps-module.nix
+++ modules/flatpak&linyaps-module.nix
@@ -22,6 +22,21 @@
     gnomeExtensions.mihomo-tray
     gnomeExtensions.paperwm
   ];
+  
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/mutter" = {
+          experimental-features = [
+            "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+            "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+            "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+            "autoclose-xwayland" # automatically terminates Xwayland if all relevant X11 clients are gone
+          ];
+        };
+      };
+    }
+  ];
 
   # 3. 国内 Flatpak 镜像源配置
   systemd.services.configure-flatpak-repo = {
