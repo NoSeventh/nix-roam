e1cf3e6da824da179c2ec64673f3b2d1ecb47c12
xuqihao <xuqihao@ihep.ac.cn>
2026-02-14 09:41:25 +0800

    update

--- configuration.nix
+++ configuration.nix
@@ -50,9 +50,9 @@
   security.rtkit.enable = true;
 
   services.logind = {
-    lidSwitch = "hibernate"; # 合盖挂起（本机swap分区不够大，无法休眠（hibernate））
+    lidSwitch = "hibernate"; # 合盖休眠
     lidSwitchExternalPower = "suspend"; # 外接电源时合盖挂起
-    lidSwitchDocked = "suspend"; # docked状态（如拓展坞）时忽略合盖
+    lidSwitchDocked = "suspend"; # docked状态（如拓展坞）时合盖挂起
   };
 
   # --- 6. 用户与安全 ---
