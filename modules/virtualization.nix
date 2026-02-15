d8778c721fb041898a10dca40b382d898a2facfe
xuqihao <xuqihao@ihep.ac.cn>
2026-02-15 17:30:56 +0800

    change wineWowPackages from stable to stableFull

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -56,7 +56,7 @@
     spice-protocol # SPICE protocol headers
     virglrenderer # Virtual GPU support
     mesa # OpenGL support for VMs
-    wineWowPackages.stable # a wine that support both 32- and 64-bit applications
+    wineWowPackages.stableFull # a wine that support both 32- and 64-bit applications
   ];
 
   # Enable necessary kernel modules for VM performance
