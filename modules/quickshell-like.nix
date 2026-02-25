75b59ef5720b9fa59fe0e63353253008d566f1cd
xuqihao <xuqihao@ihep.ac.cn>
2026-02-25 16:29:16 +0800

    update

--- modules/quickshell-like.nix
+++ modules/quickshell-like.nix
@@ -1,20 +1,21 @@
 { config, pkgs, inputs, lib, ... }: {
-  # imports = [
-  #   inputs.dms.nixosModules.default
-  # ];
-  # programs.dms-shell = {
-  #   enable = true;
-  #   quickshell.package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
-  #   systemd.enable = true;
-  #   enableSystemMonitoring = true;
-  #   enableDynamicTheming = true;
-  #   enableAudioWavelength = true;
-  #   enableVPN = true;
-  # };
+  imports = [
+    inputs.dms.nixosModules.default
+  ];
+  programs.dms-shell = {
+    enable = true;
+    quickshell.package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
+    systemd.enable = true;
+    enableSystemMonitoring = true;
+    enableDynamicTheming = true;
+    enableAudioWavelength = true;
+    enableVPN = true;
+  };
   environment.systemPackages = with pkgs; [
-    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+    # inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     # inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default
-    # noctalia-shell
+    noctalia-shell
+    dms-shell
     # ... 可能还有其他软件包
   ];
 }
