abbc81f13f4bde56cadf7f0309766c4e12bbaae6
xuqihao <xuqihao@ihep.ac.cn>
2026-02-26 16:53:34 +0800

    configure home.nix

--- home.nix
+++ home.nix
@@ -7,8 +7,10 @@
   };
   programs.git = {
     enable = true;
-    userName = "xuqihao";
-    userEmail = "xuqihao@ihep.ac.cn";
+    settings = {
+      user.name = "xuqihao";
+      user.email = "xuqihao@ihep.ac.cn";
+    };
   };
   programs.bash = {
     enable = true;
@@ -37,12 +39,6 @@
       kalibox = "distrobox enter kalibox";
       vi = "hx";
       nv = "neovide";
-
-
-
-    #   k = "kubectl";
-    #   urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-    #   urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
     };
   };
   programs.starship = {
@@ -61,10 +57,115 @@
       env.TERM = "xterm-256color";
       font = {
         size = 12;
-        color = "#ffffff";
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
     };
   };
+  programs.vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-airline
+      gruvbox
+      catppuccin-vim   # 对应 catppuccin/vim
+      vim-commentary
+    ];
+    settings = {
+      number = true;
+      relativenumber = true;
+      tabstop = 4;
+      shiftwidth = 4;
+      expandtab = true;
+      ignorecase = true;
+      smartcase = true;
+      hidden = true;
+      background = "dark";
+      # 以下选项因不在支持列表中，移入 extraConfig：
+      # autoindent, smartindent, cursorline, hlsearch, incsearch,
+      # encoding, fileencoding, noswapfile, updatetime, termguicolors
+    };
+    extraConfig = ''
+      " ==================== 移自 settings 的不受支持选项 ====================
+      set autoindent
+      set smartindent
+      set cursorline
+      set hlsearch
+      set incsearch
+      set encoding=utf-8
+      set fileencoding=utf-8
+      set noswapfile
+      set updatetime=300
+
+      " 条件启用真彩色（保持原逻辑）
+      if has("termguicolors")
+      set termguicolors
+      endif
+
+      " ==================== 原有 Vim 脚本（函数、映射等）保持不变 ====================
+      let mapleader = ","
+      syntax on
+
+      " 透明背景函数
+      function! s:apply_transparent_bg() abort
+      highlight Normal guibg=NONE ctermbg=NONE
+      highlight NonText guibg=NONE ctermbg=NONE
+      highlight LineNr guibg=NONE ctermbg=NONE
+      highlight Folded guibg=NONE ctermbg=NONE
+      highlight EndOfBuffer guibg=NONE ctermbg=NONE
+      highlight SignColumn guibg=NONE ctermbg=NONE
+      highlight CursorLineNr guibg=NONE ctermbg=NONE
+      highlight VertSplit guibg=NONE ctermbg=NONE
+      highlight TabLineFill guibg=NONE ctermbg=NONE
+      endfunction
+      autocmd ColorScheme * call s:apply_transparent_bg()
+      call s:apply_transparent_bg()
+
+      " 主题切换功能
+      let g:current_theme = 'catppuccin'
+      function! ToggleTheme() abort
+      if g:current_theme == 'catppuccin'
+      let g:gruvbox_contrast_dark = 'soft'
+      let g:gruvbox_italic = 1
+      let g:gruvbox_transparent_bg = 1
+      colorscheme gruvbox
+      let g:airline_theme = 'gruvbox'
+      let g:current_theme = 'gruvbox'
+      echo "Theme: Gruvbox (soft dark)"
+      else
+      colorscheme catppuccin_mocha
+      let g:current_theme = 'catppuccin'
+      echo "Theme: Catppuccin Mocha"
+      endif
+      endfunction
+      nnoremap <silent> <Leader>t :call ToggleTheme()<CR>
+
+      " Airline 配置
+      let g:airline_powerline_fonts = 1
+      let g:airline#extensions#tabline#enabled = 1
+      let g:airline#extensions#tabline#left_sep = ' '
+      let g:airline#extensions#tabline#left_alt_sep = '|'
+      let g:airline#extensions#tabline#formatter = 'unique_tail'
+
+      if g:current_theme == 'gruvbox'
+      let g:gruvbox_contrast_dark = 'soft'
+      let g:gruvbox_italic = 1
+      let g:gruvbox_transparent_bg = 1
+      silent! colorscheme gruvbox
+      let g:airline_theme = 'gruvbox'
+      else
+      silent! colorscheme catppuccin_mocha
+      endif
+
+      " 其他快捷键
+      nnoremap <space> :nohlsearch<CR>
+      nnoremap <F2> :set number! relativenumber!<CR>
+      nnoremap <Leader>w :w<CR>
+      nnoremap <Leader>q :q<CR>
+
+      if has("autocmd")
+      autocmd BufWritePost $MYVIMRC source $MYVIMRC
+      endif
+    '';
+  };
+  xdg.configFile."~/.config/btop/btop.conf".source = ./dotfiles/.config/btop/btop.conf;
 }
