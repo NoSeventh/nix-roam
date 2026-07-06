e4df5bf7f5ca37dde0b6698458873e986e4f7a55
xuqihao <xuqihao@ihep.ac.cn>
2026-07-06 15:57:20 +0800

    feat(flake): parameterize system, add homeConfigurations (xuqihao + darwin reserve)

--- flake.nix
+++ flake.nix
@@ -1,30 +1,10 @@
 {
-  description = "NixOS configuration with auto-module loading";
+  description = "NixOS configuration + portable Home Manager CLI environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
-
-    # zen-browser = {
-    #   url = "github:youwen5/zen-browser-flake";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
-    # quickshell = {
-    #   url = "git+https://git.outfoxxed.me/quickshell/quickshell";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
-    # dms = {
-    #   url = "github:AvengeMedia/DankMaterialShell";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
-    # noctalia = {
-    #   url = "github:noctalia-dev/noctalia-shell";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
 
     hermes-agent = {
       url = "github:NousResearch/hermes-agent";
@@ -36,12 +16,12 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # --- 3. NixVim (声明式Neovim配置) ---
+    # --- NixVim (声明式 Neovim 配置) ---
     nixvim = {
       url = "github:nix-community/nixvim/nixos-26.05";
     };
 
-    # --- 4. Chaotic AUR 源 ---
+    # --- Chaotic AUR 源 ---
     chaotic = {
       url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -59,19 +39,30 @@
       ...
     }@inputs:
     let
-      system = "x86_64-linux";
-      # 定义 stable 包的快捷方式，并开启 allowUnfree 和 permittedInsecurePackages
-      pkgs-stable = import nixpkgs-stable {
-        inherit system;
-        config.allowUnfree = true;
-        config.permittedInsecurePackages = [
-          "electron-38.8.4"
-        ];
+      # 支持的 system 列表
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # 按 system 实例化 stable / master
+      pkgsFor = system: {
+        stable = import nixpkgs-stable {
+          inherit system;
+          config.allowUnfree = true;
+          config.permittedInsecurePackages = [ "electron-38.8.4" ];
+        };
+        master = import inputs.nixpkgs-master {
+          inherit system;
+          config.allowUnfree = true;
+        };
       };
-      pkgs-master = import inputs.nixpkgs-master {
-        inherit system;
-        config.allowUnfree = true;
-      };
+
+      # NixOS 仍固定 x86_64-linux
+      nixosSystem = "x86_64-linux";
+      nixosPkgs = pkgsFor nixosSystem;
+
       # 自动扫描 modules 目录下的所有 .nix 文件
       configDir = ./modules;
       generatedModules = builtins.map (file: configDir + "/${file}") (
@@ -79,31 +70,77 @@
           builtins.attrNames (builtins.readDir configDir)
         )
       );
+
+      # 构造 standalone home-manager 配置（非 NixOS）
+      mkStandaloneHome = {
+        system,
+        homeDirectory,
+        username ? "xuqihao",
+      }:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.permittedInsecurePackages = [ "electron-38.8.4" ];
+        };
+        extra = pkgsFor system;
+      in
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = {
+            inherit inputs;
+            pkgs-stable = extra.stable;
+            pkgs-master = extra.master;
+          };
+          modules = [
+            ./home/standalone.nix
+            {
+              home = {
+                inherit username homeDirectory;
+                stateVersion = "26.05";
+              };
+            }
+          ];
+        };
     in
     {
+      # --- 1. NixOS 系统配置（x86_64-linux，行为不变） ---
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
+        system = nixosSystem;
         specialArgs = {
-          inherit inputs pkgs-stable pkgs-master;
+          inherit inputs;
+          pkgs-stable = nixosPkgs.stable;
+          pkgs-master = nixosPkgs.master;
         };
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
-          chaotic.nixosModules.default # 从 chaotic 源导入模块
+          chaotic.nixosModules.default
           inputs.hermes-agent.nixosModules.default
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.xuqihao = import ./home/default.nix;
 
-            # 使用 home-manager.extraSpecialArgs 自定义传递给 ./home.nix 的参数
-            # 取消注释下面这一行，就可以在 home.nix 中使用 flake 的所有 inputs 参数了
             home-manager.extraSpecialArgs = {
-              inherit inputs pkgs-stable pkgs-master;
+              inherit inputs;
+              pkgs-stable = nixosPkgs.stable;
+              pkgs-master = nixosPkgs.master;
             };
           }
-        ]
-        ++ generatedModules;
+        ] ++ generatedModules;
+      };
+
+      # --- 2. 非 NixOS 便携 CLI 环境（Home Manager standalone, x86_64-linux） ---
+      homeConfigurations.xuqihao = mkStandaloneHome {
+        system = "x86_64-linux";
+        homeDirectory = "/home/xuqihao";
+      };
+
+      # --- 3. macOS 预留（aarch64-darwin，仅结构就绪，未实测 build） ---
+      homeConfigurations.xuqihao-darwin = mkStandaloneHome {
+        system = "aarch64-darwin";
+        homeDirectory = "/Users/xuqihao";
       };
     };
 }
