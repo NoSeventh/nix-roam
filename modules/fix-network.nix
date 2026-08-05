6d3b9b3dd983461a11d227fbd65865113ae6b4bd
xuqihao <xuqihao@ihep.ac.cn>
2026-08-05 16:09:13 +0800

    refactor: share nix-cn settings via home/nix-cn.nix on NixOS

--- modules/fix-network.nix
+++ modules/fix-network.nix
@@ -1,24 +1,16 @@
-{ config, pkgs, lib, ... }:
+{ ... }:
 
 {
-  nix.settings = {
-    # 优先使用国内镜像站
-    substituters = lib.mkForce [
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://cache.nixos.org/"
-    ];
+  imports = [ ../home/nix-cn.nix ];
 
+  nix.settings = {
     # 增大下载缓存，防止大文件下载中断 (500MB)
     download-buffer-size = 524288000;
-    
-    connect-timeout = 5;
-    fallback = true;
-    
-     # 自动优化存储，节省空间
-     auto-optimise-store = true;
+
+    # 自动优化存储，节省空间
+    auto-optimise-store = true;
   };
- 
+
 #   nix.extraOptions = ''
 #     !include /etc/nix/github-access-tokens
 #   '';
